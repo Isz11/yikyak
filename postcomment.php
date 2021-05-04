247fe2708bd16a59e3f6fd3f82bf428598b2e7f9
Ben <ben-mcleod@live.co.uk>
2021-05-04 23:27:32 +0100

    Add page to post comment. Must fix

---
+++ postcomment.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+include('config/db_connect.php');
+
+// echo htmlspecialchars($_GET["id"]);
+// $yakid = mysqli_real_escape_string($conn, $_GET['id']);
+
+$yakid = (int) $_GET['id']; // grabs yak id from url
+// echo 'this yak id is ' . $yakid . "</br>" . 'the logged in user is ' . $_SESSION['id'] . "</br>";
+
+// var_dump($yakid);
+
+
+if(isset($_POST['submit'])){
+    if(empty($_POST['yakcomment'])){
+        echo 'Please enter a comment';
+    } else {
+        $newcomment = mysqli_real_escape_string($conn, $_POST['yakcomment']);
+        $yakid = mysqli_real_escape_string($conn, $_POST['yakid']);
+        $id = $_SESSION['id']; // this grabs the id of the logged in user
+        // var_dump($yakid);
+        $sql = "INSERT INTO comments(user,yak,comment) VALUES('$id','$yakid','$newcomment')";
+        // $yakid = $_POST['yakid'];
+        if(mysqli_query($conn, $sql)){
+          header('Location: yakcomments.php');
+        } else {
+          echo 'Query error: ' . mysqli_error($conn);
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <title>Yik Yak</title>
+    <?php include ('templates/header.php'); ?>
+    <h1>Post a comment</h1>
+
+    <form class="" action="postcomment.php" method="post">
+        <input type="hidden" name="yakid" value="<?php echo htmlspecialchars($yakid) ?>">
+        <div>
+            <textarea name="yakcomment" rows="8" cols="80" placeholder="Enter your comment here"></textarea>
+        </div>
+        <div class="">
+            <?php
+            if(isset($_SESSION["loggedin"])){ ?>
+                <input type="submit" name="submit" value="Post comment" class="btn">
+            <?php } else {
+                echo 'Please login to comment on a yak';
+            } ?>
+        </div>
+    </form>
+
+
+
+    <?php include ('templates/footer.php'); ?>
+</html>
