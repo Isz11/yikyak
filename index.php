b93881b54ce98a907a7af1f1ad99f382327d2258
Ben <ben-mcleod@live.co.uk>
2021-05-12 20:17:25 +0100

    move vote to separate file

--- index.php
+++ index.php
@@ -11,43 +11,7 @@ if(isset($_SESSION["loggedin"])){
     $id = $_SESSION['id'];
 }
 
-if(isset($_POST['upvote']) && isset($_SESSION["loggedin"])){
-    $yakid = mysqli_real_escape_string($conn, $_POST['yakid']);
-    $sql3 = "SELECT COUNT(*) FROM votes WHERE user = $id AND yak = $yakid AND vote = 1";
-    $result2 = mysqli_query($conn, $sql3);
-    $alreadyupvoted = mysqli_fetch_assoc($result2)['COUNT(*)'];
-    if(mysqli_query($conn, $sql3) && $alreadyupvoted > 0){
-        echo $alreadyupvoted . ' this is already upvoted'; //this is just for testing purposes
-    } else {
-        $sqlu1 = "INSERT INTO votes(user,yak,vote) VALUES('$id','$yakid','1')";
-        $sqlu2 = "UPDATE yaks SET score = score + 1 WHERE id = $yakid";
-        if(mysqli_query($conn, $sqlu1) && mysqli_query($conn, $sqlu2)){
-            echo 'you upvoted this yak';
-            header("Refresh:0");
-        } else {
-            echo 'Query error: ' . mysqli_error($conn);
-        }
-    }
-}
-
-if(isset($_POST['downvote']) && isset($_SESSION["loggedin"])){
-    $yakid = mysqli_real_escape_string($conn, $_POST['yakid']);
-    $sql4 = "SELECT COUNT(*) FROM votes WHERE user = $id AND yak = $yakid AND vote = -1";
-    $result3 = mysqli_query($conn, $sql4);
-    $alreadydownvoted = mysqli_fetch_assoc($result3)['COUNT(*)'];
-    if(mysqli_query($conn, $sql4) && $alreadydownvoted > 0){
-        echo $alreadydownvoted . ' this is already downvoted'; //this is just for testing purposes
-    } else {
-        $sqld1 = "INSERT INTO votes(user,yak,vote) VALUES('$id','$yakid','-1')";
-        $sqld2 = "UPDATE yaks SET score = score - 1 WHERE id = $yakid";
-        if(mysqli_query($conn, $sqld1) && mysqli_query($conn, $sqld2)){
-            echo 'you downvoted this yak';
-            header("Refresh:0");
-        } else {
-            echo 'Query error: ' . mysqli_error($conn);
-        }
-    }
-}
+include('voteyak.php');
 ?>
 
 <!DOCTYPE html>
