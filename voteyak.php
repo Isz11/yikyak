f88a46769d60c279ebc70f439de495bc7c16ee96
Ben <ben-mcleod@live.co.uk>
2021-05-13 15:38:19 +0100

    Be able to undo your vote

--- voteyak.php
+++ voteyak.php
@@ -8,20 +8,27 @@ if(isset($_POST['upvote']) && isset($_SESSION["loggedin"])){
     $alreadyupvoted1 = mysqli_fetch_assoc($resultu1)['COUNT(*)'];
     $alreadydownvoted1 = mysqli_fetch_assoc($resultu2)['COUNT(*)'];
     if($alreadyupvoted1 > 0){
-        echo $alreadyupvoted1 . ' this is already upvoted'; //this is just for testing purposes
-    } elseif($alreadydownvoted1 > 0) {
-        $sqlu3 = "UPDATE votes SET vote = 1 WHERE yak = $yakid";
-        $sqlu4 = "UPDATE yaks SET score = score + 2 WHERE id = $yakid";
+        $sqlu3 = "UPDATE votes SET vote = 0 WHERE yak = $yakid";
+        $sqlu4 = "UPDATE yaks SET score = score - 1 WHERE id = $yakid";
         if(mysqli_query($conn, $sqlu3) && mysqli_query($conn, $sqlu4)){
             echo 'you upvoted this yak';
             header("Refresh:0");
         } else {
             echo 'Query error: ' . mysqli_error($conn);
         }
-    } else {
-        $sqlu5 = "INSERT INTO votes(user,yak,vote) VALUES('$id','$yakid','1')";
-        $sqlu6 = "UPDATE yaks SET score = score + 1 WHERE id = $yakid";
+    } elseif($alreadydownvoted1 > 0) {
+        $sqlu5 = "UPDATE votes SET vote = 1 WHERE yak = $yakid";
+        $sqlu6 = "UPDATE yaks SET score = score + 2 WHERE id = $yakid";
         if(mysqli_query($conn, $sqlu5) && mysqli_query($conn, $sqlu6)){
+            echo 'you upvoted this yak';
+            header("Refresh:0");
+        } else {
+            echo 'Query error: ' . mysqli_error($conn);
+        }
+    } else {
+        $sqlu7 = "INSERT INTO votes(user,yak,vote) VALUES('$id','$yakid','1')";
+        $sqlu8 = "UPDATE yaks SET score = score + 1 WHERE id = $yakid";
+        if(mysqli_query($conn, $sqlu7) && mysqli_query($conn, $sqlu8)){
             echo 'you upvoted this yak';
             header("Refresh:0");
         } else {
@@ -39,20 +46,27 @@ if(isset($_POST['downvote']) && isset($_SESSION["loggedin"])){
     $alreadyupvoted2 = mysqli_fetch_assoc($resultd1)['COUNT(*)'];
     $alreadydownvoted2 = mysqli_fetch_assoc($resultd2)['COUNT(*)'];
     if($alreadydownvoted2 > 0){
-        echo $alreadyupvoted2 . ' this is already downvoted'; //this is just for testing purposes
-    } elseif($alreadyupvoted2 > 0) {
-        $sqld3 = "UPDATE votes SET vote = -1 WHERE yak = $yakid";
-        $sqld4 = "UPDATE yaks SET score = score - 2 WHERE id = $yakid";
+        $sqld3 = "UPDATE votes SET vote = 0 WHERE yak = $yakid";
+        $sqld4 = "UPDATE yaks SET score = score + 1 WHERE id = $yakid";
         if(mysqli_query($conn, $sqld3) && mysqli_query($conn, $sqld4)){
             echo 'you downvoted this yak';
             header("Refresh:0");
         } else {
             echo 'Query error: ' . mysqli_error($conn);
         }
-    } else {
-        $sqld5 = "INSERT INTO votes(user,yak,vote) VALUES('$id','$yakid','-1')";
-        $sqld6 = "UPDATE yaks SET score = score - 1 WHERE id = $yakid";
+    } elseif($alreadyupvoted2 > 0) {
+        $sqld5 = "UPDATE votes SET vote = -1 WHERE yak = $yakid";
+        $sqld6 = "UPDATE yaks SET score = score - 2 WHERE id = $yakid";
         if(mysqli_query($conn, $sqld5) && mysqli_query($conn, $sqld6)){
+            echo 'you downvoted this yak';
+            header("Refresh:0");
+        } else {
+            echo 'Query error: ' . mysqli_error($conn);
+        }
+    } else {
+        $sqld7 = "INSERT INTO votes(user,yak,vote) VALUES('$id','$yakid','-1')";
+        $sqld8 = "UPDATE yaks SET score = score - 1 WHERE id = $yakid";
+        if(mysqli_query($conn, $sqld7) && mysqli_query($conn, $sqld8)){
             echo 'you downvoted this yak';
             header("Refresh:0");
         } else {
