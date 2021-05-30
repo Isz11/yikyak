aefdace87c828e952d26b20b4f1ebb1a1eb43196
Ben <ben-mcleod@live.co.uk>
2021-05-30 21:01:41 +0100

    small changes for voting

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ if(isset($_SESSION["loggedin"])){
         <div>
             <?php foreach($yaks as $yak):
                 $yakid = $yak['id'];
-
+                $yakscore = intval($yak['score']);
                 // count comments for each yak
                 $sql = "SELECT COUNT(*) FROM comments WHERE yak = $yakid";
                 $result = mysqli_query($conn, $sql);
@@ -56,30 +56,27 @@ if(isset($_SESSION["loggedin"])){
 
                 echo htmlspecialchars($yak['yak']); ?><br><br></span>
                 <?php if(isset($_SESSION["loggedin"])){ ?>
-                    <form id="yak_upvote" class="" action="index.php" method="post">
-                        <input id="yakid" type="hidden" name="yakid" value="<?php echo $yak['id'] ?>">
-                        <input id="userid" type="hidden" name="userid" value="<?php echo $id ?>">
-                        <button class ="up_btn" type="submit" name="upvote">
-                            <span id="up_icon"><i class="fas fa-angle-up"></i></span>
-                        </button>
-                    </form>
-                    <form id="yak_downvote" class="" action="index.php" method="post">
-                        <input id="yakid" type="hidden" name="yakid" value="<?php echo $yak['id'] ?>">
-                        <input id="userid" type="hidden" name="userid" value="<?php echo $id ?>">
-                        <button class ="down_btn" type="submit" name="downvote">
-                            <span id="down_icon"><i class="fas fa-angle-down"></i></span>
-                        </button>
-                    </form>
+
+                    <button data-id="<?php echo $yak['id']; ?>" class ="up_btn" type="button" name="upvote">
+                        <span id="up_icon"><i class="fas fa-angle-up"></i></span>
+                    </button>
+
+                    <button data-id="<?php echo $yak['id']; ?>" class ="down_btn" type="button" name="downvote">
+                        <span id="down_icon"><i class="fas fa-angle-down"></i></span>
+                    </button>
                 <?php } ?>
-                    <span class="votescore">
-                        <?php
-                        echo intval($yak['score']). ' karma'; ?><br><?php
-                        echo get_time_ago(strtotime($yak['created'])); ?><br><?php
-                        echo $comments;  ?>
-                        <a href="yakcomments.php?id=<?php echo $yak['id'] ?>">Comments</a><br><br><br><br>
-                <?php endforeach; ?>
+                <span id="<?php echo 'scoreOf'.$yak['id']; ?>" class="votescore">
+                    <?php echo $yakscore; ?>
+                </span><br><?php
+                    echo get_time_ago(strtotime($yak['created'])); ?><br><?php
+                    echo $comments;  ?>
+                    <a href="yakcomments.php?id=<?php echo $yak['id'] ?>">Comments</a><br><br><br><br>
+            <?php endforeach; ?>
         </div>
     </ul>
+    <script type="text/javascript">
+        let userid = "<?php echo $id; ?>";
+    </script>
     <script type="text/javascript" src="sandbox.js"></script>
     <?php include ('templates/footer.php'); ?>
 </html>
