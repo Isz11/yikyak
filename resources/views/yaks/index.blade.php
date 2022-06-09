d5f37464ed08ddb4a56b4444429c3e96a5d584f2
Ben Mcleod <ben-mcleod@live.co.uk>
2022-06-09 17:12:49 +0100

    Minor layout change

--- resources/views/yaks/index.blade.php
+++ resources/views/yaks/index.blade.php
@@ -3,44 +3,42 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <p>{{ session('mssg') }}</p>
-        <div class="row">
-            <h4 class="yak-title">All the yaks</h4><br>
-        </div>
-    <div>
+        <h4 class="yak-title">All the yaks</h4><br>
+    </div>
         @foreach($yaks as $yak)
             
             <div class="col-md-20">
                 <div class="card">
-                    <div class="row">
-                        <div class="row justify-content-start">
-                            <h6>Posted by {{ $yak->user->username }}</h6>
-                        </div>
-
+                    <div class="card-body">
                         <div class="row">
-                            @livewire('yak-scores', ['yak' => $yak])
-                        </div>
-                        <div>
-                            <div class="col align-self-end">
-                                <h4><a href="/yaks/{{ $yak->id }}">{{ $yak->yak }}</a></h4>
+                            <div class="col-md-1">
+                                @livewire('votes', ['yak' => $yak])
                             </div>
-                        </div>
 
-                        <div class="row justify-content-start">
-                            {{ get_time_ago($yak->created_at) }} ---
+                            <div class="col">
+                                <div class="row">
+                                    <h6>Posted by {{ $yak->user->username }}</h6>
+                                </div>
+                                <div class="row">
+                                    <h4><a href="/yaks/{{ $yak->id }}">{{ $yak->yak }}</a></h4>
+                                </div>
+                                <div class="row">
+                                    {{ get_time_ago($yak->created_at) }} ---
 
-                            @php $count = 0 @endphp
-                            @foreach($replies as $reply)
-                                @if ($reply->yak_id == $yak->id)
-                                    @php $count++ @endphp
-                                @endif
-                            @endforeach
+                                    @php $count = 0 @endphp
+                                    @foreach($replies as $reply)
+                                        @if ($reply->yak_id == $yak->id)
+                                            @php $count++ @endphp
+                                        @endif
+                                    @endforeach
 
-                            @if ($count == 1)
-                                {{ $count }} reply
-                            @else
-                                {{ $count }} replies
-                            @endif
+                                    @if ($count == 1)
+                                        {{ $count }} reply
+                                    @else
+                                        {{ $count }} replies
+                                    @endif
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
