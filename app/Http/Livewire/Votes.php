321793cfb3d0860f1bf318a1b58c545876e8d3fe
Ben Mcleod <ben-mcleod@live.co.uk>
2022-06-09 22:32:56 +0100

    Livewire voting component

--- app/Http/Livewire/Votes.php
+++ app/Http/Livewire/Votes.php
@@ -10,12 +10,28 @@ class Votes extends Component
 {
 
     public $yak;
-    public $totalScore;
+    public $total_score;
+    public $arrow_up = 'black';
+    public $arrow_down = 'black';
 
     public function mount($yak)
     {
         $this->yak = $yak;
-        $this->totalScore = $yak->score;
+        $this->total_score = $yak->score;
+
+        if (Vote::where('yak_id', $this->yak->id)
+            ->where('user_id', auth()->id())
+            ->where('vote', 1)
+            ->first() != null) {
+            $this->arrow_up = 'orange';
+        }
+
+        if (Vote::where('yak_id', $this->yak->id)
+            ->where('user_id', auth()->id())
+            ->where('vote', -1)
+            ->first() != null) {
+            $this->arrow_down = 'orange';
+        }
     }
 
     public function render()
@@ -23,20 +39,47 @@ class Votes extends Component
         return view('livewire.votes');
     }
 
-    public function vote($vote)
+    public function vote($vote) //need to sort the readability of this function out
     {
-        if (Vote::where('yak_id', $this->yak->id)
+        $existing_vote = Vote::where('yak_id', $this->yak->id)
             ->where('user_id', auth()->id())
-            ->where('vote', $vote)
-            ->count()) {
-            $this->totalScore += $vote;
+            ->first();
+
+        if ($existing_vote != null) {
+            if ($existing_vote->vote == $vote) {
+                $this->total_score += $vote * -1;
+                Vote::where('id', $existing_vote->id)
+                ->update([
+                    'vote' => 0
+                ]);
+                $this->arrow_up = 'black';
+                $this->arrow_down = 'black';
+            } else {
+                $this->total_score += $vote - $existing_vote->vote;
+                Vote::where('id', $existing_vote->id)
+                ->update([
+                    'vote' => $vote
+                ]);
+                if ($vote === 1) {
+                    $this->arrow_up = 'orange';
+                    $this->arrow_down = 'black';
+                } else {
+                    $this->arrow_up = 'black';
+                    $this->arrow_down = 'orange';
+                }
+            }
         } else {
-            $this->totalScore += $vote;
+            $this->total_score += $vote;
             Vote::create([
                 'yak_id' => $this->yak->id,
                 'user_id' => auth()->id(),
                 'vote' => $vote
             ]);
+            if ($vote === 1) {
+                $this->arrow_up = 'orange';
+            } else {
+                $this->arrow_down = 'orange';
+            }
         }
     }
 }
--- resources/views/livewire/votes.blade.php
+++ resources/views/livewire/votes.blade.php
@@ -2,17 +2,17 @@
     <div class="text-center">
         <div>
             @auth
-            <a wire:click.prevent="vote(1)" href="#">
+            <a wire:click.prevent="vote(1)" href="#" style="color: {{ $arrow_up }}">
             @endauth
-                <i class="fa fa-2x fa-sort-asc" aria-hidden="true"></i>
+                <i class="fa fa-2x fa-sort-asc blackiconcolor" aria-hidden="true"></i>
             </a>
         </div>
-        <div>{{ $totalScore }}</div>
+        <div>{{ $total_score }}</div>
         <div>
             @auth
-            <a wire:click.prevent="vote(-1)" href="#">
+            <a wire:click.prevent="vote(-1)" href="#" style="color: {{ $arrow_down }}">
             @endauth
-                <i class="fa fa-2x fa-sort-desc" aria-hidden="true"></i>
+                <i class="fa fa-2x fa-sort-desc blackiconcolor" aria-hidden="true"></i>
             </a>
         </div>
     </div>
