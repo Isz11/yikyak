4f56acf17d17d42c6aae5133ef0883e2f1689224
Ben Mcleod <ben-mcleod@live.co.uk>
2022-06-13 16:49:34 +0100

    Spinner loading component

---
+++ resources/views/livewire/spinner.blade.php
@@ -0,0 +1,10 @@
+<div wire:loading class="loading text-center">
+    <div>
+        <div class="d-flex justify-content-center">
+            <div class="spinner-border mt-4 mb-2" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+        <p>Loading...</p>
+    </div>
+</div>
