346c7f39e038ef023533c58ac4ebdd4ba1066d52
Ben Mcleod <ben-mcleod@live.co.uk>
2022-06-13 09:45:56 +0100

    Move style colour out of auth

--- resources/views/livewire/votes.blade.php
+++ resources/views/livewire/votes.blade.php
@@ -2,17 +2,17 @@
     <div class="text-center">
         <div>
             @auth
-            <a wire:click.prevent="vote(1)" href="#" style="color: {{ $arrow_up }}">
+            <a wire:click.prevent="vote(1)" href="#">
             @endauth
-                <i class="fa fa-2x fa-sort-asc blackiconcolor" aria-hidden="true"></i>
+                <i class="fa fa-2x fa-sort-asc" style="color: {{ $arrow_up }}" aria-hidden="true"></i>
             </a>
         </div>
         <div style="color: {{ $vote_colour }}; font-weight: bold">{{ $total_score }}</div>
         <div>
             @auth
-            <a wire:click.prevent="vote(-1)" href="#" style="color: {{ $arrow_down }}">
+            <a wire:click.prevent="vote(-1)" href="#">
             @endauth
-                <i class="fa fa-2x fa-sort-desc blackiconcolor" aria-hidden="true"></i>
+                <i class="fa fa-2x fa-sort-desc" style="color: {{ $arrow_down }}" aria-hidden="true"></i>
             </a>
         </div>
     </div>
