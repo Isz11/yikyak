111d31e724330e519a5f57a8c09833bdea34abe7
Ben Mcleod <ben-mcleod@live.co.uk>
2022-06-24 11:35:00 +0100

    Add basic livewire notification dropdown

--- resources/views/livewire/notifications.blade.php
+++ resources/views/livewire/notifications.blade.php
@@ -1,3 +1,20 @@
 <div>
-    Notifications
+    <!-- Notifications from user id {{ Auth::user()->username }} -->
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            <i class="fa fa-bell fa" aria-hidden="true"></i>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">
+                Notification 1
+            </a>
+            <a class="dropdown-item" href="#">
+                Notification 2
+            </a>
+            <a class="dropdown-item" href="#">
+                Notification 3
+            </a>
+        </div>
+    </li>
 </div>
