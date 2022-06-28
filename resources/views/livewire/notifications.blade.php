9a8cd62525b4a72e9145bd84ac737d7c03c55317
Ben Mcleod <ben-mcleod@live.co.uk>
2022-06-28 10:54:15 +0100

    Show unread notifications with a highlight

--- resources/views/livewire/notifications.blade.php
+++ resources/views/livewire/notifications.blade.php
@@ -1,18 +1,26 @@
 <div>
-    <!-- Notifications from user id {{ Auth::user()->username }} -->
     <li class="nav-item dropdown">
         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             <i class="fa fa-bell fa" aria-hidden="true"></i>
-            <span class="badge badge-light">{{ auth()->user()->notifications->count() }}</span>
+            @if (auth()->user()->unreadNotifications->count() > 0)
+                <span class="badge badge-pill badge-danger">{{ auth()->user()->unreadNotifications->count() }}</span>
+            @endif
         </a>
 
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            @foreach (auth()->user()->notifications as $notification)
-                <a class="dropdown-item" href="{{ $notification->data['info']['url'] }}">
+            @foreach (auth()->user()->unreadNotifications as $notification)
+                <button style="background-color: lightgray" class="dropdown-item" href="#" wire:click.prevent="markAsRead( '{{ $notification->id }}', '{{ $notification->data['info']['url'] }}' )">
                     {{ $notification->data['info']['message'] }}
                     <br>
                     {{ get_time_ago($notification->created_at) }}
-                </a>
+                </button>
+            @endforeach
+            @foreach (auth()->user()->readNotifications as $notification)
+                <button class="dropdown-item" href="{{ $notification->data['info']['url'] }}">
+                    {{ $notification->data['info']['message'] }}
+                    <br>
+                    {{ get_time_ago($notification->created_at) }}
+                </button>
             @endforeach
         </div>
     </li>
